d76bc930e1f71aabf0e8b3eef2e1984aab0b9844
abendema <demajjj123@gmail.com>
2026-01-08 11:00:12 -0500

    changed side-bar menu style. And Toggle header icon to X in JS

--- side-bar.js
+++ side-bar.js
@@ -1,4 +1,14 @@
-function toggleElement(id){
+function toggleElement(id, icon){
   const element = document.getElementById(id);
   element.classList.toggle('active');
+
+  // toggle hamburger/X icon
+  if(icon.classList.contains('fa-bars')){
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
+  } else{
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+  }
+
 }
